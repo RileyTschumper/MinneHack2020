3ad921bfa0bfb6eeecc5c53b2e12c615db70e643
Peter Farley <far.peter1@gmail.com>
2020-01-25 22:25:55 -0600

    Added functionality to ArtworkData to calculate and compare key points. [UNTESTED]

--- BackendServer/BackendDataFormat.py
+++ BackendServer/BackendDataFormat.py
@@ -1,3 +1,5 @@
+import cv2
+
 class ArtistData:
     # Full name of artist:
     artistName = ""
@@ -12,20 +14,47 @@ class ArtistData:
 
 class ArtworkData:
     # UUID of artwork:
-    artworkID       = ""
+    artworkID        = ""
     # Name of artwork:
-    artworkName     = ""
+    artworkName      = ""
     # UUID of artist:
-    artistID        = ""
+    artistID         = ""
     # Date of creation:
-    artworkDate     = ""
+    artworkDate      = ""
     # Location of artwork:
-    artworkLocation = "" # Should this be string?
-    # Features of artwork:
-    artworkFeatures = None
+    artworkLocation  = "" # Should this be string?
+    # Key points:
+    artworkKeyPoints = None
+    # Key point descriptors:
+    artworkDescriptors = None
     # Relative location (within `images`) of image file:
-    artworkImage    = ""
+    artworkImage     = ""
     
     def __init__(self, artworkName, artistID):
         self.artworkName = artworkName
         self.artistID    = artistID
+    
+    def generateKeyPoints(self):
+        image = cv2.imread('../images/%s'%(self.artworkImage))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        sift = cv2.xfeatures2d.SIFT_create()
+
+        self.artworkKeyPoints, artworkDescriptors = sift.detectAndCompute(image, None)
+
+    def compareKeyPoints(self, otherImage):
+        image = cv2.imread(otherImage)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        sift = cv2.xfeatures2d.SIFT_create()
+
+        keyPoints, descriptors = sift.detectAndCompute(image, None)
+
+        bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+        index_params = dict(algorithm = 0, trees = 5)
+        search_params = dict()
+
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        matches = flann.knnMatch(self.artworkDescriptors, descriptors, k=2)
