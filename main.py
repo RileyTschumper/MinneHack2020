618b0ab86cfc2939f12d55224e5d80bc8767558c
Mitchell Nelson <nels4929@stthomas.edu>
2020-01-25 20:03:43 -0600

    Images folder and main

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2 
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+#from IPython import get_ipython
+#get_ipython().run_line_magic('matplotlib', 'inline')
+#exec(%matplotlib inline)
+# read images
+img1 = cv2.imread('images/mona1.jpeg')  
+img2 = cv2.imread('images/mona2.jpeg') 
+
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+#sift
+sift = cv2.xfeatures2d.SIFT_create()
+
+keypoints_1, descriptors_1 = sift.detectAndCompute(img1,None)
+keypoints_2, descriptors_2 = sift.detectAndCompute(img2,None)
+
+#feature matching
+bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+matches = bf.match(descriptors_1,descriptors_2)
+matches = sorted(matches, key = lambda x:x.distance)
+
+img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2, matches[:50], img2, flags=2)
+plt.imshow(img3),plt.show()
