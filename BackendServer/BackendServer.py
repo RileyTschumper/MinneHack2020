1a9ae21574aff6e7c0c2719a1487d24122088d19
Peter Farley <far.peter1@gmail.com>
2020-01-25 20:34:53 -0600

    Server serving static page.

--- BackendServer/BackendServer.py
+++ BackendServer/BackendServer.py
@@ -1,3 +1,17 @@
 from flask import Flask, escape, request
 
-def initServer():
+flaskApp = None
+
+def init():
+    global flaskApp
+
+    flaskApp = Flask(__name__)
+
+    @flaskApp.route("/")
+    def rootRoute():
+        return "Nothing here..."
+
+def start():
+    global flaskApp
+    
+    flaskApp.run()
---
+++ backendServerTest.py
@@ -0,0 +1,4 @@
+from BackendServer import BackendServer
+
+BackendServer.init()
+BackendServer.start()
