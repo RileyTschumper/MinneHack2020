f4bf26dd23f398a9e141bdd47448ca81f1d9371e
Peter Farley <far.peter1@gmail.com>
2020-01-25 23:01:10 -0600

    Added functionality to import JSON artwork data. [UNTESTED]

--- BackendServer/BackendDatabase.py
+++ BackendServer/BackendDatabase.py
@@ -1,3 +1,6 @@
+import json
+import random
+
 from BackendServer import BackendDataFormat
 
 class BackendDatabase:
@@ -5,11 +8,39 @@ class BackendDatabase:
     artworks = []
 
     def __init__(self):
-        pass
+        random.seed()
     
-    def loadJSONDB(self, jsonFile):
-        pass
+    def loadArtistJSON(self, jsonFile):
+        data = json.loads(jsonFile)
         # TODO: Load JSON file and populate self.artists and self.artworks
+
+    def loadArtworkJSON(self, jsonFile):
+        data = json.loads(jsonFile)
+        for rawArtwork in data:
+            if not "artworkName" in rawArtwork:
+                rawArtwork["artworkName"] = "(unnamed)"
+            if not "artworkID" in rawArtwork:
+                rawArtwork["artworkID"] = random.randint(1, 99999999)
+            if not "artistID" in rawArtwork:
+                print(" -- Artist ID must be defined!!! -- ")
+                continue
+            if not "artworkDate" in rawArtwork:
+                rawArtwork["artworkDate"] = "Undated"
+            if not "artworkLocation" in rawArtwork:
+                rawArtwork["artworkLocation"] = "No location"
+            if not "artworkImage" in rawArtwork:
+                print(" -- Artwork image file path must be defined!")
+                continue
+
+            artwork = BackendDataFormat.ArtworkData(rawArtwork[artworkName], rawArtwork[artistID])
+            artwork.artworkID       = rawArtwork["artworkID"]
+            artwork.artworkDate     = rawArtwork["artworkDate"]
+            artwork.artworkLocation = rawArtwork["artworkLocation"]
+            artwork.artworkImage    = rawArtwork["artworkImage"]
+            artwork.generateKeyPoints()
+
+            self.artworks.append(artwork)
+        pass
 
     def getArtistByID(self, artistID):
         for artist in self.artists:
